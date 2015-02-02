73461cd1fa23c7b1145851c7cb011fb86801945c
Gawkat <hp_johannes@hotmail.com>
2015-02-02 21:45:25 +0100

    First commit.

---
+++ desktop/src/com/github/gawkat/tanks/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.github.gawkat.tanks.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.github.gawkat.tanks.TanksGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TanksGame(), config);
+	}
+}
