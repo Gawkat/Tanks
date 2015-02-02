3b59d1294d91ff3b8a7135632594a21adadf8756
Gawkat <hp_johannes@hotmail.com>
2015-02-02 22:01:06 +0100

    setup

--- core/src/com/github/gawkat/tanks/TanksGame.java
+++ core/src/com/github/gawkat/tanks/TanksGame.java
@@ -3,25 +3,41 @@ package com.github.gawkat.tanks;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.Texture.TextureFilter;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator.FreeTypeFontParameter;
 
 public class TanksGame extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
-	
+
+	BitmapFont font12;
+
 	@Override
-	public void create () {
+	public void create() {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+
+		// Text Test
+		FreeTypeFontGenerator generator = new FreeTypeFontGenerator(
+				Gdx.files.internal("font.ttf"));
+		FreeTypeFontParameter parameter = new FreeTypeFontParameter();
+		parameter.size = 12;
+		font12 = generator.generateFont(parameter); // font size 12
+													// pixels
+		font12.getRegion().getTexture()
+				.setFilter(TextureFilter.Linear, TextureFilter.Linear);
+		generator.dispose(); // don't forget to dispose to avoid memory leaks!
 	}
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+	public void render() {
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+
+		font12.draw(batch, "Version 0.1a", 10, Gdx.graphics.getHeight() - 10);
+
 		batch.end();
 	}
 }
--- desktop/src/com/github/gawkat/tanks/desktop/DesktopLauncher.java
+++ desktop/src/com/github/gawkat/tanks/desktop/DesktopLauncher.java
@@ -5,8 +5,14 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.github.gawkat.tanks.TanksGame;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
+	public static void main(String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+		config.title = "Tanks";
+		config.width = 800;
+		config.height = 600;
+		config.resizable = false;
+
 		new LwjglApplication(new TanksGame(), config);
 	}
 }
